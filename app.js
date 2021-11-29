0957145b2ccd465cb1fb2654e5717e32236cf0b5
Alissa Seixas <alissaps@gmail.com>
2021-11-29 19:47:29 -0300

    initial commit

--- app.js
+++ app.js
@@ -1,12 +1,64 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const PORT = 4000;
 
 // Importing all the pokemon for our data file
 const allPokemon = require("./data");
+const postRouter = express.Router();
 
 const app = express();
 
-// -- Define your route listeners here! --
+app.use(express.json());
+app.use("/", postRouter);
 
-app.listen(PORT, () => console.log(`Server up and running at port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server rodando na porta ${PORT}`);
+});
+
+// 01
+
+postRouter.get("/pokemon", (req, res) => {
+  return res.status(200).json({ ...allPokemon });
+});
+
+// 02
+postRouter.get("/pokemon/:id", (req, res) => {
+  const indexToSubstitute = allPokemon.findIndex((currentPost) => {
+    console.log(currentPost.id == req.params.id)
+    return currentPost.id == req.params.id;
+  });
+  
+   res.status(200).json({ ...allPokemon[indexToSubstitute] });
+});
+
+// 04
+postRouter.post("/pokemon", (req, res) => {
+  allPokemon.push({ id: uuidv4(), ...req.body });
+  return res.status(201).json({ ...allPokemon[allPokemon.length - 1] });
+});
+
+// 05
+
+postRouter.put("/pokemon/:id", (req, res) => {
+  const indexToSubstitute = allPokemon.findIndex((currentPost) => {
+    console.log(currentPost.id == req.params.id)
+    return currentPost.id == req.params.id;
+  });
+
+  allPokemon[indexToSubstitute] = { ...allPokemon[indexToSubstitute], ...req.body };
+
+  res.status(200).json({ ...allPokemon[indexToSubstitute] });
+});
+
+// 06
+
+postRouter.delete("/pokemon/:id", (req, res) => {
+  allPokemon.map((currentPost, i) => {
+    if (currentPost.id === req.params.id) {
+      allPokemon.splice(i, 1);
+    }
+  });
+
+  res.status(200).json({});
+});
